1e895cee7c1519f87ada4c8f31e193d695e4d904
Léo Garret <leodu34@hotmail.com>
2019-01-26 21:13:48 +0100

    Test
    Testing git

---
+++ Ghost in the House/Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void credits()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
