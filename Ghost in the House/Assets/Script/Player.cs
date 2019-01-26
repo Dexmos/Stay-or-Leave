609fa74d90fdc35f1f471dc0755aca6e212fe37c
Alexis <alexis.wirth@epitech.eu>
2019-01-26 19:50:55 +0100

    push house nb 1
    pushing scene for useless pnj's houses

---
+++ Ghost in the House/Assets/Script/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text countCoin;
+
+    public Text finalText;
+    private Rigidbody rb;
+    private int nb_destroy;
+    private int nb_coins;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        nb_destroy = 0;
+        nb_coins = 0;
+        countText.text = "Task: " + nb_destroy.ToString () + "/4";
+        countCoin.text = "Coins: " + nb_coins.ToString();
+
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            nb_destroy += 1;
+            nb_coins += 100;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("money"))
+        {
+            other.gameObject.SetActive(false);
+            nb_coins += 1000;
+            SetCountText();
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text = "Task: " + nb_destroy.ToString() + "/4";
+        countCoin.text = "Coins: " + nb_coins.ToString();
+    }
+
+
+}
